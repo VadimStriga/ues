c43a8b1b70612288f11e26d7319436bfb0f4c1fa
Vadim Striga <undey86@yandex.ru>
2024-05-07 11:54:56 +0700

    DjDT

--- ues/ues/settings.py
+++ ues/ues/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'counterparties.apps.CounterpartiesConfig',
     'users.apps.UsersConfig',
+    'debug_toolbar',
 ]
 
 AUTH_USER_MODEL = 'users.User'
@@ -51,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'ues.urls'
--- ues/ues/urls.py
+++ ues/ues/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('counterparties.urls', namespace='counterparties')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
