57d8c9404b482464d2886d8dd96a09700217ec6a
Vadim Striga <undey86@yandex.ru>
2025-04-09 10:24:48 +0700

    add STATIC_ROOT to settings.py

--- ues/ues/settings.py
+++ ues/ues/settings.py
@@ -156,6 +156,7 @@ DATE_FORMAT = 'd.m.Y'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
 MEDIA_URL = '/media/'
